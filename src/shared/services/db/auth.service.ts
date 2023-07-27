9e45f3566e9e7508dd0dfea3236aebc0deadf6e5
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2023-07-27 22:00:00 +0100

    clean up

--- src/shared/services/db/auth.service.ts
+++ src/shared/services/db/auth.service.ts
@@ -2,41 +2,71 @@ import { IAuthDocument } from "@auth/interfaces/auth.interface";
 import { AuthModel } from "@auth/models/auth.schema";
 
 export async function createAuthUser(data: IAuthDocument): Promise<void> {
-    await AuthModel.create(data)
+  await AuthModel.create(data);
 }
 
-export async function getUserByUsernameOrEmail (username: string, email: string): Promise<IAuthDocument> {
-    const query = {
-        $or: [{ username}, {email}]
+export async function getUserByUsernameOrEmail(
+  username: string,
+  email: string
+): Promise<IAuthDocument> {
+  const query = {
+    $or: [{ username }, { email }],
+  };
+  const user: IAuthDocument = (await AuthModel.findOne(
+    query
+  ).exec()) as IAuthDocument;
+  return user;
+}
+
+export const getAuthUserByUsername = async (
+  username: string
+): Promise<IAuthDocument> => {
+  const user: IAuthDocument = (await AuthModel.findOne({
+    username,
+  }).exec()) as IAuthDocument;
+  return user;
+};
+
+export const getUserByEmail = async (email: string): Promise<IAuthDocument> => {
+  const user: IAuthDocument = (await AuthModel.findOne({
+    email,
+  }).exec()) as IAuthDocument;
+  return user;
+};
+
+export async function updatePasswordToken(
+  authId: string,
+  token: string,
+  tokenExpiration: number
+): Promise<void> {
+  await AuthModel.updateOne(
+    { _id: authId },
+    {
+      passwordResetToken: token,
+      passwordResetExpires: tokenExpiration,
     }
-    const user: IAuthDocument = await AuthModel.findOne(query).exec() as IAuthDocument;
-    return user;
+  );
 }
 
-export const getAuthUserByUsername = async (username: string): Promise<IAuthDocument> => {
-    const user: IAuthDocument = (await AuthModel.findOne({ username }).exec()) as IAuthDocument;
+export async function getAuthUserByToken(token: string) {
+  const user: IAuthDocument = (await AuthModel.findOne({
+    passwordResetToken: token,
+    passwordResetExpires: { $gt: Date.now() },
+  }).exec()) as IAuthDocument;
+  return user;
+}
+
+export async function getAuthUserByEmail(email: string) {
+  const user: IAuthDocument = (await AuthModel.findOne({
+    email,
+  }).exec()) as IAuthDocument;
+  return user;
+}
+
+export async function getAuthUserByPasswordToken(token: string): Promise<IAuthDocument> {
+    const user: IAuthDocument = (await AuthModel.findOne({
+      passwordResetToken: token,
+      passwordResetExpires: { $gt: Date.now() }
+    }).exec()) as IAuthDocument;
     return user;
   }
-
-
-  export const getUserByEmail = async (email: string): Promise<IAuthDocument> => {
-    const user: IAuthDocument = (await AuthModel.findOne({ email }).exec()) as IAuthDocument;
-    return user;
-  }
-
-  export async function updatePasswordToken(authId: string, token: string, tokenExpiration: number): Promise<void> {
-    await AuthModel.updateOne({_id: authId}, {
-        passwordResetToken: token,
-        passwordResetExpires: tokenExpiration
-    })
-  }
-
-  export async function getAuthUserByToken(token:string) {
-      const user: IAuthDocument = ( await AuthModel.findOne({ 
-        passwordResetToken: token,
-        passwordResetExpires: { $gt: Date.now()}
-      }).exec()) as IAuthDocument; 
-      return user;
-  }
-
-
