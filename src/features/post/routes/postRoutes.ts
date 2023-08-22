be393fc46b4b8ad6c5a434e8070580ec1ffe75ea
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2023-08-22 15:30:38 +0100

    post Routes

---
+++ src/features/post/routes/postRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { Create } from "@post/controllers/post.controller";
+import { checkAuthentication } from "@global/helpers/auth-middleware";
+const router = Router();
+
+
+router.route("/").post(checkAuthentication, Create.prototype.post);
+router.route("/image").post(checkAuthentication, Create.prototype.PostwithImage);
+
+
+export const PostRouter = router;
