b6e5e43c0c66de5ea0c8ac91c43b8648dddd13cd
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2023-08-22 15:30:00 +0100

    post router

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { AuthRouter } from "@auth/routes/authRoutes";
 import { currentRouter } from "@auth/routes/currentRoutes";
 import { verifyUser } from "@global/helpers/auth-middleware";
+import { PostRouter } from "@post/routes/postRoutes";
 import { serverAdapter } from "@services/queues/base.queue";
 import { Application } from "express";
 
@@ -10,4 +11,5 @@ export default (app:Application) => {
     app.use("/queues", serverAdapter.getRouter())
     app.use(`${BASE_URL}/auth`, AuthRouter)
     app.use(`${BASE_URL}`, verifyUser, currentRouter);
+    app.use(`${BASE_URL}/post`, PostRouter);
 }
