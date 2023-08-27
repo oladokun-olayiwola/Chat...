af8814eccce65fc4f62ce6c7fc68e553e2141435
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2023-08-27 18:47:24 +0100

    add post to database

--- src/shared/workers/post.worker.ts
+++ src/shared/workers/post.worker.ts
@@ -1,6 +1,7 @@
 import { DoneCallback, Job } from "bull";
 import Logger from "bunyan";
 import { createLogger } from "@global/helpers/logger";
+import { postService } from "@services/db/post.service";
 const log: Logger = createLogger("Post Worker");
 
 export const savePostToDb = async (
@@ -8,7 +9,8 @@ export const savePostToDb = async (
   done: DoneCallback
 ): Promise<void> => {
   try {
-    // const { value, key } = job.data;
+    const { value, key } = job.data;
+    await postService.addPostToDB(value, key);
     job.progress(100);
     done(null, job.data);
   } catch (error) {
